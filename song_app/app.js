a81fd1eb648fa5239ec0d8e79a4f65f9ad7e3902
YG777 <katyake@gamil.com>
2018-04-07 18:19:28 +0100

    ejs laouts and minor corrections

--- song_app/app.js
+++ song_app/app.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var app = express();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //check this
+app.use(bodyParser.urlencoded({ extended: false })); //check this
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 app.use('/', routes);
 
 
