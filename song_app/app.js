b6f38a9c83434824d3bb8fd99a2587a6ef07a5e0
YG777 <katyake@gamil.com>
2018-04-08 23:36:15 +0100

    set up gulp tasks and made minor structureal changes

--- song_app/app.js
+++ song_app/app.js
@@ -4,7 +4,8 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const routes = require('./config/routes');
+const {port} = require('./config/environment');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
-app.use('/', routes);
+app.use(routes);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -33,7 +34,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-
 app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
@@ -41,5 +41,4 @@ app.use(function(err, req, res) {
     error: {}
   });
 });
-
-module.exports = app;
+app.listen(port, () => console.log(`Express up and running on port: ${port}`));
--- song_app/gulpfile.js
+++ song_app/gulpfile.js
@@ -1,11 +1,9 @@
 const gulp        = require('gulp');
 const sass        = require('gulp-sass');
+const nodemon     = require('gulp-nodemon');
 const cleanCSS    = require('gulp-clean-css');
 const plumber     = require('gulp-plumber');
-
-gulp.task('default', function() {
-  console.log('gulp says hello');
-});
+const browserSync   = require('browser-sync').create();
 
 gulp.task('sass', () => {
   return gulp.src('src/scss/**/*.scss')
@@ -13,4 +11,26 @@ gulp.task('sass', () => {
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('public/css'));
+    
 });
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    proxy: 'http://localhost:3000',
+    port: 8000,
+    files: ['public/**/*.*'],
+    reloadDelay: 500,
+    notify: false,
+    open: true
+  });
+
+  return nodemon({ script: 'app.js'})
+    .on('start', () => browserSync.reload());
+});
+
+gulp.task('default', ['sass', 'serve'], () => {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('views/**/*.ejs', browserSync.reload);
+});
+
+
